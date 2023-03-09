fab648b7d8c6b8910d3b94ca5acbe44fd065b354
qwertyforce <qwertyforce@protonmail.com>
2023-03-09 14:26:17 +0300

    add delete by file name

--- global_features_web.py
+++ global_features_web.py
@@ -32,7 +32,7 @@ import pickle
 index = None
 DATA_CHANGED_SINCE_LAST_SAVE = False
 
-from modules.byte_ops import int_to_bytes
+from modules.byte_ops import int_to_bytes, int_from_bytes
 from modules.inference_ops import get_image_features, get_device
 from modules.transform_ops import transform
 from modules.lmdb_ops import get_dbs
@@ -83,6 +83,13 @@ def get_filenames_bulk(image_ids):
 
     return file_names
 
+def get_image_id_by_filename(file_name):
+    with DB_filename_to_id.begin(buffers=True) as txn:
+        image_id = txn.get(file_name.encode(), default=False)
+        if not image_id:
+            return False
+        return int_from_bytes(image_id)
+    
 def delete_descriptor_by_id(image_id):
     image_id_bytes = int_to_bytes(image_id)
     with DB_features.begin(write=True, buffers=True) as txn:
@@ -235,19 +242,24 @@ async def calculate_global_features_handler(image: bytes = File(...),image_id: s
         traceback.print_exc()
         raise HTTPException(status_code=500, detail="Can't calculate global features")
 
-class Item_image_id(BaseModel):
-    image_id: int
+class Item_delete_global_features(BaseModel):
+    image_id: Union[int ,None] = None
+    file_name: Union[None,str] = None
 
 @app.post("/delete_global_features")
-async def delete_global_features_handler(item:Item_image_id):
+async def delete_global_features_handler(item:Item_delete_global_features):
     try:
         global DATA_CHANGED_SINCE_LAST_SAVE
-        res = index.remove_ids(np.int64([item.image_id]))
+        if item.file_name:
+            image_id = get_image_id_by_filename(item.file_name)
+        else:
+            image_id = item.image_id
+        res = index.remove_ids(np.int64([image_id]))
         if res != 0: 
-            delete_descriptor_by_id(item.image_id)
+            delete_descriptor_by_id(image_id)
             DATA_CHANGED_SINCE_LAST_SAVE = True
         else: #nothing to delete
-            print(f"err: no image with id {item.image_id}")    
+            print(f"err: no image with id {image_id}")    
         return Response(status_code=status.HTTP_200_OK)
     except:
         traceback.print_exc()
